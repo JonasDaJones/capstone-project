f23993c5510c327d128ee0683c044913e0b5c5ff
Jonas Laß <johnny.jones@gmx.net>
2023-07-06 11:22:37 +0200

    link to prdal form

---
+++ pages/new-pedal.js
@@ -0,0 +1,13 @@
+import React from "react";
+import NewPedalForm from "../components/new-pedal-form";
+
+export default function AddPedal() {
+  return (
+    (<header>New Pedal \o/</header>),
+    (
+      <main>
+        <NewPedalForm />
+      </main>
+    )
+  );
+}
